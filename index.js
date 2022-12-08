66dfb41179268c06d4e0b40718ece2a4855e0fec
Vendone <andreasventurin@gmail.com>
2022-12-08 08:09:22 +0100

    git ignore geändert

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).send({ msg: "Pro Darts League Backend" });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on PORT ${port}`);
 });
