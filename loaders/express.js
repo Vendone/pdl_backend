77c80d8e849ce0aba17e61ef3a04be6559b56274
Vendone <andreasventurin@gmail.com>
2023-02-12 09:41:20 +0100

    datenbank erstellung ergänzt

--- loaders/express.js
+++ loaders/express.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: false,
+        secure: true,
         maxAge: 24 * 60 * 60 * 1000,
       },
       store: new pgSession({
