7da74d1dd4e10f1248534089bed55ed3efa8daa9
Vendone <andreasventurin@gmail.com>
2023-02-26 08:45:52 +0100

    Left Join für user_stats eingebaut

--- helperFunctions/index.js
+++ helperFunctions/index.js
@@ -2,12 +2,11 @@ const db = require("../db");
 const bcrypt = require("bcrypt");
 
 async function findOneByEmail(email) {
-  const statement = `SELECT *
-FROM users
-WHERE email = $1`;
+  const statement = `SELECT users.id, users.email, users.password, users.firstname, users.lastname, users.nickname, users.img, 
+  users.userlvl, user_stats.dart, user_stats.dart_weight, user_stats.nation, user_stats.ligaid
+FROM users LEFT JOIN user_stats ON users.id = user_stats.userid WHERE email = $1 ORDER BY users.id`;
   const values = [email];
   const user = await db.query(statement, values);
-
   if (!user) {
     return done(null, false, {
       message: "Incorrect username or password.",
@@ -17,9 +16,9 @@ WHERE email = $1`;
 }
 
 async function findOneById(id) {
-  const statement = `SELECT *
-  FROM users
-  WHERE id = $1`;
+  const statement = `SELECT users.email, users.password, users.firstname, users.lastname, users.nickname, users.img, 
+  users.userlvl, user_stats.dart, user_stats.dart_weight, user_stats.nation, user_stats.ligaid
+FROM users LEFT JOIN user_stats ON users.id = user_stats.userid WHERE users.id = $1 ORDER BY users.id`;
   const values = [id];
   const user = await db.query(statement, values);
 
