6973a2f659cb89a8ed2a8311deb8f232533d097b
Vendone <andreasventurin@gmail.com>
2023-08-28 09:07:17 +0200

    Left Join zur abfrage der stats eingebaut

--- routes/player.js
+++ routes/player.js
@@ -9,7 +9,12 @@ module.exports = (app) => {
   app.use("/api/player", router);
 
   router.get("/", async (req, res, next) => {
-    const statement = `SELECT firstname, lastname, nickname,img FROM users`;
+    const statement = `
+    SELECT 
+    users.firstname, users.lastname, users.nickname, users.img,
+    stats.games, stats.win, stats.lose, stats.tie, stats.points, stats.differenz, stats.dart, stats.dart_weight, stats.nation, stats.ligaid
+    FROM users 
+    LEFT JOIN user_stats AS stats ON users.id = stats.userid`;
     const player = await db.query(statement);
     if (!player) {
       return done(null, false, {
