c473f7fd07911539f7a3b33bf2d1cfe060cb4e7d
Vendone <andreasventurin@gmail.com>
2023-08-29 11:41:10 +0200

    get abfrage von einelnen usern eingebaut

--- routes/player.js
+++ routes/player.js
@@ -24,4 +24,20 @@ module.exports = (app) => {
 
     res.send(player.rows);
   });
+
+  router.get("/:id", async (req, res, next) => {
+    const statement = `
+    SELECT 
+    users.id, users.firstname, users.lastname, users.nickname, users.img,
+    stats.games, stats.win, stats.lose, stats.tie, stats.points, stats.differenz, stats.dart, stats.dart_weight, stats.nation, stats.ligaid
+    FROM users LEFT JOIN user_stats AS stats ON users.id = stats.userid WHERE users.id = 7`;
+    const player = await db.query(statement);
+    if (!player) {
+      return done(null, false, {
+        message: "no Player",
+      });
+    }
+
+    res.send(player.rows);
+  });
 };
