9c7f0186a9d1357a5a11421b60d375013e044d8a
Vendone <andreasventurin@gmail.com>
2023-08-29 11:43:45 +0200

    id ist dynamisch

--- routes/player.js
+++ routes/player.js
@@ -26,11 +26,12 @@ module.exports = (app) => {
   });
 
   router.get("/:id", async (req, res, next) => {
+    const id = req.params.id;
     const statement = `
     SELECT 
     users.id, users.firstname, users.lastname, users.nickname, users.img,
     stats.games, stats.win, stats.lose, stats.tie, stats.points, stats.differenz, stats.dart, stats.dart_weight, stats.nation, stats.ligaid
-    FROM users LEFT JOIN user_stats AS stats ON users.id = stats.userid WHERE users.id = 7`;
+    FROM users LEFT JOIN user_stats AS stats ON users.id = stats.userid WHERE users.id = ${id}`;
     const player = await db.query(statement);
     if (!player) {
       return done(null, false, {
