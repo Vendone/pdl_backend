0b4d3e5fe2a0ae2e03defe3e9eb41a4026689577
Vendone <andreasventurin@gmail.com>
2023-09-09 07:44:47 +0200

    Spieler abfrage sortiert nach firstname absteigend

--- routes/player.js
+++ routes/player.js
@@ -14,7 +14,7 @@ module.exports = (app) => {
     users.id, users.firstname, users.lastname, users.nickname, users.img,
     stats.games, stats.win, stats.lose, stats.tie, stats.points, stats.differenz, stats.dart, stats.dart_weight, stats.nation, stats.ligaid
     FROM users 
-    LEFT JOIN user_stats AS stats ON users.id = stats.userid`;
+    LEFT JOIN user_stats AS stats ON users.id = stats.userid ORDER BY users.firstname ASC`;
     const player = await db.query(statement);
     if (!player) {
       return done(null, false, {
